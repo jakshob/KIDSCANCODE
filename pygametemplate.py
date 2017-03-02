889595ccc4d52a656ee3035c9aafca924e54eadb
jakshob <jakshob@hotmail.com>
2017-03-02 13:12:37 +0100

    new files!

--- pygametemplate.py
+++ pygametemplate.py
@@ -21,12 +21,25 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Jakobs spil")
 clock = pygame.time.Clock()
 
+all_sprites = pygame.sprite.Group()
 #Game loop
 
 running = True
 
 while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
     # Process input (events)
+    for event in pygame.event.get():
+        #Check for closing the window
+        if event.type == pygame.QUIT:
+            running = False
     # Update
+    all_sprites.update()
     # Draw / render
-    screen.fill()
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # *after* Drawing everything, flip the display
+    pygame.display.flip()
+
+pygame.quit()
